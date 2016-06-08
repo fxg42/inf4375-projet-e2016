03be7d83eb789eb372b3aa750241909107427e61
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2016-06-08 08:15:00 -0400

    fetch quotes

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,25 @@
-$(function(){
-  console.log('Ça marche')
-})
+var renderCitation = function (citation) {
+  return '<li>'+ citation.contenu +' &mdash;'+ citation.auteur +'</li>'
+}
+
+var renderCitations = function (citations) {
+  return '<ul>'+ citations.map(renderCitation).join('') +'</ul>'
+}
+
+var mountListeCitations = function (citations) {
+  document.getElementById('liste-citations').innerHTML = renderCitations(citations)
+}
+
+var fetchAll = function () {
+  var xhr = new XMLHttpRequest()
+  xhr.open('get', '/citations', true)
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var citations = JSON.parse(xhr.responseText)
+      mountListeCitations(citations)
+    }
+  }
+  xhr.send()
+}
+
+document.addEventListener('DOMContentLoaded', fetchAll)
